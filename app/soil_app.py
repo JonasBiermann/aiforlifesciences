1dc08dc1f7d69d0d1a7c204c6b88f7d0263d33c6
jonasmac <jojabiermann@gmail.com>
2023-11-22 22:42:44 +0100

    added country mapping and input fields for user

--- app/soil_app.py
+++ app/soil_app.py
@@ -19,6 +19,14 @@ df['K']=df['K'].replace("< LOD",-1).replace("<0.0",0).astype(float)
 
 df['SURVEY_DATE'] = pd.to_datetime(df['SURVEY_DATE'],format="%d-%m-%y")
 
+country_codes = pd.read_html('https://en.wikipedia.org/wiki/Nomenclature_of_Territorial_Units_for_Statistics')[0]['Countries']
+country_codes.columns = ['country','code']
+country_mapping = dict(zip(country_codes['code'],country_codes['country']))
+country_mapping['EL'] = 'Greece'
+country_mapping['UK'] = 'United Kingdom'
+
+df['COUNTRY'] = df['NUTS_0'].map(country_mapping)
+
 st.title("Lucas Soil Dataset Analysis")
 st.subheader("Exploring Soil Characteristics and Distribution")
 st.write("This project aims to utilize the LUCAS soil dataset to analyze the relationship between soil chemistry and soil biodiversity. The project will focus on understanding how the distribution of different soil nutrients and properties, such as pH, electrical conductivity (EC), organic carbon (OC), and inorganic nitrogen (N) affects the abundance and diversity of soil microorganisms. The project will also investigate the potential for soil chemistry to serve as an early warning system for the emergence of soil-borne pathogens, therefore making significant contributions to our understanding of soil health and the challenges facing agricultural soils. It will also provide valuable insights for policymakers and land managers who are working to improve soil health and protect the environment.")
@@ -46,7 +54,7 @@ selected_options = st.multiselect('', countries)
 if "All" in selected_options:
     selected_options = countries[1:] # selected_options keeps track of which countries are selected
 
-df = df[
+df_map = df[
     (df['SURVEY_DATE']>=start_date) & 
     (df['SURVEY_DATE']<=end_date) &
     (df['pH_CaCl2'] >= ph_val[0]) & 
@@ -64,5 +72,24 @@ df = df[
     (df['K'] >= kal_val[0]) &
     (df['K'] <= kal_val[1])
 ]
+if 'All' not in selected_options:
+    df_map = df_map[(df_map['COUNTRY'].isin(selected_options))]
 
-st.map(df, latitude='TH_LAT', longitude="TH_LONG", use_container_width=True)
+st.map(df_map, latitude='TH_LAT', longitude="TH_LONG", use_container_width=True)
+
+st.subheader('Check your own values!')
+st.write('We trained our Machine Learning model so that you can check the biodiversity of your soil based on your own chemical measurements. Just insert your values below and we\'ll see what your soil is made of and how you could possibly improve it\'s conditions.')
+
+col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 1, 1, 1])
+
+columns = [(col1, 'pH_CaCl2'), (col2, 'EC'), (col3, 'OC'), (col4, 'CaCO3'), (col5, 'P'), (col6, 'N'), (col7, 'K')]
+
+def create_input_field(column, label):
+    with column:
+        st.number_input(label, min_value=min(df[label]), max_value=max(df[label]), value=df[label].mean())
+
+for col in columns:
+    create_input_field(col[0], col[1])
+
+if st.button('Check Your Soil Data!'):
+    st.write('test')
