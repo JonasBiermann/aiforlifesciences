52f6ba0fe5a85fdba38230176d1cd0529c962d0a
jonasmac <jojabiermann@gmail.com>
2023-12-13 22:52:23 +0100

    changed lucas soil name

--- app/dataframe.py
+++ app/dataframe.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib as plt
 import matplotlib.colors as mcolors
 
-df1 = pd.read_csv('artifacts/LUCAS SOIL 2018.csv')
+df1 = pd.read_csv('artifacts/lucas_soil_2018.csv')
 df2 = pd.read_csv('artifacts/Shannon.csv')
 df = pd.merge(df1, df2, on='POINTID', how='outer', suffixes=('', '_y'))
 df.drop(df.filter(regex='_y$').columns, axis=1, inplace=True)
